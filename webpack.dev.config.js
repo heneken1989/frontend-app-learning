fff671183f0a8057a975293ea5d6adc92c84a225
heneken1989 <huyhien1989gmail.com>
2025-10-24 21:59:00 +0700

    dd

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,33 +5,18 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = createConfig('webpack-dev');
 
-// GHI ĐÈ splitChunks config từ @openedx/frontend-build
+// TẮT HOÀN TOÀN CODE SPLITTING - Gộp tất cả thành 1 file
 config.optimization.splitChunks = {
-  chunks: "all",
-  minSize: 100000,        // Tăng từ 20KB lên 100KB
-  maxSize: 500000,        // Tăng từ 244KB lên 500KB
-  maxAsyncRequests: 5,    // Giới hạn async chunks
-  maxInitialRequests: 3,  // Giới hạn initial chunks
+  chunks: 'all',
   cacheGroups: {
-    // Gộp TẤT CẢ vendor libraries thành 1 file lớn
-    vendor: {
-      test: /[\\/]node_modules[\\/]/,
-      name: 'vendors',
+    // Gộp TẤT CẢ thành 1 file duy nhất
+    default: {
+      name: 'bundle',
       chunks: 'all',
-      priority: 20,
+      priority: 1,
       enforce: true,
-      minSize: 0,         // Bỏ qua minSize cho vendor
-      maxSize: 0,         // Không giới hạn kích thước
-    },
-    // Gộp code chung thành 1 file
-    common: {
-      name: 'common',
-      minChunks: 2,
-      chunks: 'all',
-      priority: 10,
-      reuseExistingChunk: true,
-      enforce: true,
-      minSize: 50000,     // Chỉ tách khi >= 50KB
+      minSize: 0,
+      maxSize: 0,
     },
   },
 };
